f09c7d78de18de7d34b3b8927d996d1b3f2d21e4
Marcos García <marcosluisgm@gmail.com>
2018-01-20 18:32:38 +0100

    Search cities by type

--- app.js
+++ app.js
@@ -11,12 +11,17 @@ app.get('/', function (req, res) {
 });
 
 
-app.post('/cities', function (req, res) {
-  var isSpain = false
+app.post('/api/cities', function (req, res) {
+  var isSpain = req.body.result.parameters.isSpain === 'España'
+  var type = req.body.result.parameters.tipoCiudad
   
-  var functionName = 'getBestBeachCities'
-  // var functionName = 'getBestNaturalParksCities'
-  // var functionName = 'getBestSportCities'
+  var functionName = 'getBestCities';
+  if (type.indexOf('ciudad') > -1) functionName = 'getBestCities'
+  if (type.indexOf('playa') > -1) functionName = 'getBestBeachCities'
+  if (type.indexOf('montaña') > -1) functionName = 'getBestMountainCities'
+  if (type.indexOf('rural') > -1) functionName = 'getBestRuralCities'
+  
+  console.log(req.body.result.parameters)
 
   return minube[functionName](isSpain)
     .then(cities => res.json(cities))
